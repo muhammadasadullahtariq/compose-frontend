9468c7b71c2688baeb252807acc4c0a6491e23a3
Abdul Wahab <abdulwahabdev0@gmail.com>
2023-07-26 17:32:57 +0500

    update responsive

--- src/components/question2.js
+++ src/components/question2.js
@@ -4,12 +4,27 @@ import { questions3 } from "@/constants/questions";
 
 const Question2 = () => {
   return (
-    <Grid container>
+    <Grid container sx={{ gap: { lg: "20px", xs: "10px" } }}>
       {questions3.map((item) => (
-        <Grid item key={item} lg={2} xs={12} md={4} sm={6}>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <Grid item key={item} lg={2} xs={12} md={3} sm={6}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <Image src={item.image} alt={item} />
-            <Typography as="p" sx={{ textAlign: 'center', width: '100%', fontWeight: "500" }}>
+            <Typography
+              as="p"
+              sx={{
+                textAlign: "center",
+                width: "100%",
+                fontWeight: "500",
+                marginTop: "7px",
+              }}
+            >
               {item.title}
             </Typography>
           </div>
